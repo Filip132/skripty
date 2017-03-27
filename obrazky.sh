c6eb5a8fa9645da925055a9b3d32c2e4fa57babc
Filip132 <klapfilip@seznam.cz>
2017-03-27 08:33:43 +0200

    Add files via upload

---
+++ obrazky.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+function Menu  {
+echo "||-------------MENU-------------------------------||"
+echo "||--------a) konverze obrazku      ---------------||"
+echo "||--------b) zmena kvality obrazku ---------------||"
+echo "||------- c) zmena rozmeru obrazku ---------------||"
+echo "||--------d) otoceni obrazku       ---------------||"
+echo "||--------e) efekty                ---------------||"
+echo "||------------------------------------------------||"
+echo $volba
+}
+
+function efekty {
+echo "||-------------Efekty-----------------------------||"
+echo "||--------a) efekt charchoal ---------------------||"
+echo "||--------b) efekt implode   ---------------------||"
+echo "||------------------------------------------------||"
+}
+
+function kvalita {
+echo"
+
